9afc022c7b0f7b999d4cd3b81a2eb807bee743be
Soreine <soreine.plume@gmail.com>
2016-03-14 14:35:15 +0100

    Adds octicons

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-var classes = {
+var CLASSES = {
     note:'info',
     tip:'success',
     warning:'danger',
     working:'warning'
+};
+
+function makeIcon(name) {
+    return '<i class="octicon octicon-'+name+'"></i>';
+}
+var ICONS = {
+    note: '',
+    tip: makeIcon('mortar-board'), // Diploma hat
+    warning: makeIcon('issue-opened'),
+    working: makeIcon('tools')
 };
 
 module.exports = {
@@ -11,7 +21,8 @@ module.exports = {
             process: function (block) {
                 // Available styles: note, warning, tip, working
                 var style = block.kwargs.style || 'note';
-                return '<div class="alert alert-'+classes[style]+'">'
+                return '<div class="alert alert-'+CLASSES[style]+'">'
+                    + ICONS[style]
                     + block.body
                     + '</div>';
             }
