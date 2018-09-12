895178b3a85c484de56060cecbe1fec14893a387
Simran Brucherseifer <simran@arangodb.com>
2018-09-12 23:30:18 +0200

    Account for line break at beginning of block body

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ module.exports = {
         hint: {
             process: function (block) {
                 var type = types[block.args[0]] || types['info'];
-                var firstLineIndent = block.body.match(/^[ ]*/)[0].length;
+                var firstLineIndent = block.body.match(/^\n?([ ]*)/)[1].length;
                 var unindentRegex = new RegExp('^[ ]{0,' + firstLineIndent + '}', 'mg');
                 var body = block.body.replace(unindentRegex, '');
 
